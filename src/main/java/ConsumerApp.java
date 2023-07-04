f6e6f95a4636fffa49f0c0328f5a891252a1e351
Elimane Sall <salleli@ept.sn>
2023-07-04 20:36:14 +0000

    First Commit

---
+++ src/main/java/ConsumerApp.java
@@ -0,0 +1,68 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.example.Climat;
+import org.example.ClimatImpl;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        ClimatImpl climat = new ClimatImpl();
+
+
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"true");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group-1");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+        kafkaConsumer.subscribe(Collections.singletonList("weather"));
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(()->{
+            System.out.println("-----------------------------------");
+            ConsumerRecords<String, String> consumerRecord = kafkaConsumer.poll(Duration.ofMinutes(15));
+            consumerRecord.forEach(cr->{
+                JSONObject json = new JSONObject(cr.value());
+
+// Extraire les valeurs des différentes clés de l'objet JSON
+                Long id = null; // Remplacez par la valeur appropriée si nécessaire
+                Timestamp sunrise = Timestamp.valueOf(json.getString("sunrise"));
+                int visibility = json.getInt("visibility");
+                Timestamp timezone = Timestamp.valueOf(json.getString("timezone"));
+                String weatherMain = json.getString("weatherMain");
+                BigDecimal temperatureMax = new BigDecimal(json.getString("temperatureMax"));
+                Timestamp timeCollecte = Timestamp.valueOf(json.getString("timeCollecte"));
+                BigDecimal temperatureMin = new BigDecimal(json.getString("temperatureMin"));
+                int pressure = json.getInt("pressure");
+                Timestamp currentTime = Timestamp.valueOf(json.getString("currentTime"));
+                int seaLevel = json.getInt("seaLevel");
+                Timestamp sunset = Timestamp.valueOf(json.getString("sunset"));
+                BigDecimal temperature = new BigDecimal(json.getString("temperature"));
+                int humidity = json.getInt("humidity");
+                int windDirection = json.getInt("windDirection");
+                int grndLevel = json.getInt("grndLevel");
+                BigDecimal windSpeed = new BigDecimal(json.getString("windSpeed"));
+
+
+                System.out.println("Key=>"+cr.key()+"value=>"+cr.value() +"offsef=>"+cr.offset());
+            });
+
+
+//            climat.saveClimat(climat1);
+        },0, 15, TimeUnit.MINUTES);
+    }
+}
